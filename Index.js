0ce0fce91cf094ace53058763750750110c9de4a
wiilliam24 <yeremtravi@gmail.com>
2025-02-17 22:39:42 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,18 +1,13 @@
 // index.js
 
-// Importamos las funciones desde tasks.js
 const { addTask, listTasks, deleteTask, updateTask } = require("./tasks");
 
-// Agregamos tareas
 addTask("Aprender Node.js");
 addTask("Practicar JavaScript");
 addTask("Construir un proyecto");
 
-// Mostramos la lista de tareas
 listTasks();
 
-// Actualizamos la segunda tarea (índice 1)
 updateTask(1, "Practicar JavaScript avanzado");
 
-// Mostramos la lista de tareas actualizada
 listTasks();
